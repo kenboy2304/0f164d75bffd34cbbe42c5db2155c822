1d147c8a28899e11dc06265d8c75f132a30458f3
vietpham <kenboy2304@gmail.com>
2014-05-21 09:33:39 +0700

    The First Source

---
+++ CDNVN.MediaSocialNetwork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CDNVN.MediaSocialNetwork.Startup))]
+namespace CDNVN.MediaSocialNetwork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
